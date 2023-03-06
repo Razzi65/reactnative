04401b346512035d1c6b4ced7177b9bb1a4d6f37
Razzi65 <rizmoh65@gmail.com>
2023-03-06 23:05:44 +0500

    react Native

---
+++ App.tsx
@@ -0,0 +1,16 @@
+import { View, Text } from "react-native"
+
+const app = () => {
+
+  return (
+    <View>
+<Text>
+
+
+</Text>
+
+    </View>
+  )
+}
+
+export default app
---
+++ components/counterApp.tsx
@@ -0,0 +1,13 @@
+import { View } from "react-native/types"
+
+
+const CounterApp = () => {
+
+    return (
+        <View>
+                <Inp
+        </View>
+    )
+}
+
+export default CounterApp
