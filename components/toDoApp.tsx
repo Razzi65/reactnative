4ccbc6f4597e2cda50d35ccf2bad32c7f7e1f653
Razzi65 <rizmoh65@gmail.com>
2023-03-31 03:09:29 +0500

    todo with redux toolkit

--- components/toDoApp.tsx
+++ components/toDoApp.tsx
@@ -5,30 +5,21 @@ const ToDoApp = () => {
 
     const {onPressHandler, userInput, setUserInput, userToDo, onDelHandler} = UseToDo()
 
-   
-        
-    
-
     return (
         <View>
-           
-                <TextInput style={{borderColor:"black", borderWidth:2 }} placeholder="write here" defaultValue={userInput}
-                onChangeText={text=>setUserInput(text)}/>
-                <Button onPress={onPressHandler} title="add"/> 
+            <TextInput style={{borderColor:"black", borderWidth:2 }} placeholder="write here" defaultValue={userInput}
+                onChangeText={text=>setUserInput(text)} />
+            <Button onPress={onPressHandler} title="add" />
 
-             {userToDo.map((item, index)=>{
+            {userToDo.map((item, index)=>{
                 return (
-                    <View key={index}>
-                    <Text >  {item.toDoItem} </Text>
-                    <Button onPress={()=>onDelHandler(index)} title="Del"/> 
+                    <View key={index} style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between', marginVertical: 4 }}>
+                        <Text style={{ fontSize: 18 }}>{item.toDoItem}</Text>
+                        <Button onPress={()=>onDelHandler(index)} title="Del" color="red" />
                     </View>
                 )
-               })} 
-             
-
+            })}
         </View>
-        
-
     )
 }
 
--- custom hooks/useToDo.tsx
+++ custom hooks/useToDo.tsx
@@ -1,30 +1,72 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import ToDoType from "../types/toDoType"
+import firestore from '@react-native-firebase/firestore';
+
 
 
 const UseToDo = () => {
+
+
 
     const [userInput, setUserInput] = useState("")
     const [userToDo, setUserToDo] = useState<ToDoType[]>([])
 
 
-    const addedToDo:ToDoType = {
-        toDoItem:userInput
+    const addedToDo: ToDoType = {
+        toDoItem: userInput
     }
 
+    const getTodos = async () => {
+        try {
+            const toDoFirebase = await firestore().collection('todos').get();
+            console.log(toDoFirebase.docs);
+            
+            
+            
+        } catch (error) {
+            console.log(error);
+            
+            
+        }
+    }
+
+    useEffect(()=>{getTodos()},[])
 
     const onPressHandler = () => {
-        setUserToDo([...userToDo, addedToDo])
-        setUserInput("")
+        
+        try {
+
+            firestore()
+                .collection('todos')
+                .add({
+                    toDoItem: userInput,
+                    
+                })
+                .then((docref) => {
+                    console.log('User added!');
+                    console.log(docref.id);
+                    
+                });
+
+
+
+
+        } catch (error) {
+            console.log(error);
+
+
+        }
     }
 
-    const onDelHandler = (index) => {
-          const filtered = userToDo.filter((val, ind)=>index!=ind)  
-          setUserToDo(filtered)
+
+
+    const onDelHandler = (index: number) => {
+        const filtered = userToDo.filter((val, ind) => index != ind)
+        setUserToDo(filtered)
     }
 
-    
-   
+
+
 
     return {
         onPressHandler, userInput, setUserInput, userToDo, onDelHandler
